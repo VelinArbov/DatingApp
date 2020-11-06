eccaf158a2cc6d2d95c6d58f769b28bebc61c682
Velin Arbov <48315855+VelinArbov@users.noreply.github.com>
2020-11-06 15:59:46 +0200

    Create Edit Page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
@@ -13,8 +14,9 @@ import {NotFoundComponent} from './_interceptors/not-found/not-found.component'
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'members',component: MemberListComponent,canActivate: [AuthGuard]},
+  {path:'members',component: MemberListComponent},
   {path:'members/:username',component: MemberDetailComponent,canActivate: [AuthGuard]},
+  {path:'member/edit',component: MemberEditComponent,canActivate: [AuthGuard]},
   {path:'lists',component: ListsComponent,canActivate: [AuthGuard]},
   {path:'messages',component: MessagesComponent,canActivate: [AuthGuard]},
   
